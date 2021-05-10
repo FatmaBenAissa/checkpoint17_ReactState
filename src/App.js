0c2a81e20843c1b88e9426b234e58bee255a5e42
fatma <fatmabenaissa1@gmail.com>
2021-05-10 10:36:07 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import { Component } from 'react';
+import image1 from "./im1.png"
+
+class App extends Component{
+
+  constructor(props){
+    console.log("constructor()");
+    super(props);
+    this.state={
+      intervall:null,
+      timer:0
+    }
+
+  }
+  componentDidMount(){
+    console.log("componentDidMount()");
+    this.setState({
+      intervall:setInterval(()=>{
+        this.setState({timer:this.state.timer+1})
+      },1000)
+    });
+  }
+  componentDidUpdate(){
+    console.log("componentDidUpdate()");
+  }
+  
+  componentWillUnmount(){
+    console.log("componentWillUnmount()")
+    clearInterval(this.state.intervall);
+  }
+
+state={isVisible:false}
+person=[
+  'Fatma Ben Aissa',
+  'i have no idea 4 bio',
+  'PHD student',
+
+]
+showV=()=>{
+  this.setState({
+    isVisible:!this.state.isVisible,
+    timer:null
+  
+  })
+  
 }
+
+render(){
+  return (
+    <div>
+    
+<center>
+{this.state.isVisible ? (
+<>   <button onClick={this.showV}>Hide info</button>
+    {this.person.map((p,i)=>(
+      
+      
+      <h3 key={i}>{p}</h3>
+     
+    )
+
+    )
+    }
+     <center>
+     <img src={image1} style={{width:350,heigth:400, marginBottom:40}}/></center>
+     <center><input type="text" value={this.state.timer} ></input></center>
+    </>
+    ) 
+    :(<button onClick={this.showV}>Show info</button>
+       
+     
+       )}
+
+</center>
+
+
+    </div>)
+    
+ 
+  
+}
+}
+
+
 
 export default App;
